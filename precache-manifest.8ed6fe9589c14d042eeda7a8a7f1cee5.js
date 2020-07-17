7f8c8cd55a16fb853c4a430f2c25ae72d6c552f1
nybik1 <spudegbutman@gmail.com>
2020-07-17 18:21:27 +0300

    Updates

---
+++ precache-manifest.8ed6fe9589c14d042eeda7a8a7f1cee5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5270ec1ab29ab66289368e44d40a2cb2",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "a3948f6a694d768565b4",
+    "url": "/MovieDB/static/css/2.4b39fc77.chunk.css"
+  },
+  {
+    "revision": "e565d66db56afcb93dcc",
+    "url": "/MovieDB/static/css/main.3ce03dc5.chunk.css"
+  },
+  {
+    "revision": "a3948f6a694d768565b4",
+    "url": "/MovieDB/static/js/2.71499431.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/MovieDB/static/js/2.71499431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e565d66db56afcb93dcc",
+    "url": "/MovieDB/static/js/main.ea49ed4e.chunk.js"
+  },
+  {
+    "revision": "d402b411a98894dd7c56",
+    "url": "/MovieDB/static/js/runtime-main.2859c1ef.js"
+  },
+  {
+    "revision": "f5c3cdf5d48acdde238dd23569c9397e",
+    "url": "/MovieDB/static/media/back.f5c3cdf5.svg"
+  }
+]);
